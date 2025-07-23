79afaef0e7a892007c131f8c0f6b8dae7e99e4c9
mraihanaditya <m.raihanaditya.h@gmail.com>
2025-07-23 13:37:16 +0700

    add deployment

--- main.py
+++ main.py
@@ -73,3 +73,7 @@ def predict(payload: ImagePayload):
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Terjadi kesalahan: {str(e)}")
+    
+    
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5024)
